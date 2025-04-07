f3e83b67422aa3a306199c666db06f022c2ffe75
FIONNOROURKE-STUDENT <G00429672@atu.ie>
2025-04-07 11:13:50 +0100

    customer class

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Customer {
+    public String firstName;
+    public String lastName;
+    public String customerID;
+
+    public String adress;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
